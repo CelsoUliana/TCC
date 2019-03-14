bab1fafa933cdb8aa1fbd266e88e7a209197fd92
Celso Antonio Uliana Junior <32890738+CelsoUliana@users.noreply.github.com>
2019-03-14 20:04:34 -0300

    ajuste dnv

--- parser/parserlinux.pl
+++ parser/parserlinux.pl
@@ -10,6 +10,7 @@ my $csvFileName = shift;
 my $geojsonFileName = "geojson" . "$csvFileName" . ".json";
 my %dataHash;
 my $i = 1;
+my $aux;
 
 open CSV, "$csvFileName" or die "Couldn't open the file $csvFileName\n";
 
@@ -20,6 +21,7 @@ while(<CSV>){
     
     if($i != 1 and $i % 100 == 0){
         $dataHash{$i} = "{\n\t\t\"type\": \"Feature\",\n\t\t\"geometry\": {\n\t\t\t\"type\": \"Point\",\n\t\t\t\"coordinates\": [$fields[12], $fields[13]]\n\t\t}\n\t},\n\t\t";
+        $aux = $i;
     }
 
 	$i = $i + 1;
@@ -27,7 +29,7 @@ while(<CSV>){
 
 close CSV;
 
-$dataHash{$i} = substr $dataHash{$i}, 0, -6;
+$dataHash{$aux} = substr $dataHash{$i}, 0, -6;
 
 
 open(my $fh, '>', $geojsonFileName) or die "Could not open file '$geojsonFileName' $!";
