c48e083ce9e3b38aec630ec0a1f605a9761e998a
Wesley Campagna <wesley.campagna@hotmail.com>
2019-09-30 15:56:46 -0400

    remove and put layer animal

---
+++ parser/parserLsAno.py
@@ -0,0 +1,54 @@
+''' 
+Celso Antonio - September 2019 
+'''
+
+import csv
+import json
+
+my_json = {
+    'type' : 'FeatureCollection',
+    'features' : 
+    [
+
+    ]
+}
+
+coordArray = {}
+ 
+with open('datasetnormalizado.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        key = str(row['AnimalID']) + str(row['Ano']) + str(row['ID'])
+        a = float(row['Latitude'])
+        b = float(row['Longitude'])
+        if not key in coordArray.keys():
+            coordArray[key] = {
+                'type' : 'Feature',
+                'geometry' : 
+                {
+                    'type' : 'LineString',
+                    'coordinates' :
+                    [
+
+                    ]
+                },
+                'properties' : {
+                    'ID' : row['ID'],
+                    'Ano' : row['Ano'],
+                    'animalID' : row['AnimalID'],
+                }
+            }
+            coordArray[key]['geometry']['coordinates'].append([b , a])
+        else:
+            coordArray[key]['geometry']['coordinates'].append([b , a])
+
+''' my_json['features'].insert(0, obj feature)    '''
+'''   Tipo des  obj features diferentes, polygon, polyline, line  '''
+#my_json['features'][0]['geometry']['coordinates'] = coordArray
+
+for keys in coordArray:
+    my_json['features'].append(coordArray[keys])
+    #print(coordArray[keys])
+
+with open('test.json', 'w') as json_file:
+    json.dump(my_json, json_file)
